5bf934693b27f596834b27fb32c3dcce367d04a0
Piotrek <Swistekxd@gmail.com>
2024-02-06 02:56:01 +0100

    1

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -7,9 +7,12 @@ export const maxDuration = 5;
 
 export async function POST(req: Request, res: Response) {
   console.log("im in login ");
+
   await connectMongoDb();
   const body: UserDocument = await req.json();
   try {
+    console.log(body);
+    console.log(body.email);
     const user = await UserService.findUser({ email: body.email });
     if (user) {
       return NextResponse.json({ user }, { status: 200 });
